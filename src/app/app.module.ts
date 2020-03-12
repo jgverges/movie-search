763a6631c2ce3c7ad2d016d4bc0defcb36858737
jgverges <55912813+jgverges@users.noreply.github.com>
2020-03-12 19:54:47 +0100

    create search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { SelectorTitleComponent } from './selector-title/selector-title.component';
 import { SearchResultComponent } from './search-result/search-result.component';
+import { SearchComponent } from './search/search.component';
 /* import { from } from 'rxjs';
  */
 @NgModule({
   declarations: [
     AppComponent,
     SelectorTitleComponent,
-    SearchResultComponent
+    SearchResultComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
